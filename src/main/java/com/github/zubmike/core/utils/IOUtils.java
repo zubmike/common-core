83b80c448b0d5e079c8d78553d5bc7961568cae1
zubmike <zubmichael@gmail.com>
2022-02-23 15:51:32 +0300

    update io utils

--- src/main/java/com/github/zubmike/core/utils/IOUtils.java
+++ src/main/java/com/github/zubmike/core/utils/IOUtils.java
@@ -1,16 +1,22 @@
 package com.github.zubmike.core.utils;
 
+import javax.validation.constraints.Null;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
 import java.util.Base64;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 
 public class IOUtils {
 
-	public static <T extends Serializable> byte[] serialize(Serializable object) {
-		try (ByteArrayOutputStream byteOutputStream = new ByteArrayOutputStream();
-		     ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteOutputStream)) {
+	private static final int DEFAULT_BYTE_BUFFER_SIZE = 4096;
+
+	private static final char[] HEX_VALUES = "0123456789abcdef".toCharArray();
+
+	public static <T extends Serializable> byte[] serialize(T object) {
+		try (var byteOutputStream = new ByteArrayOutputStream();
+		     var objectOutputStream = new ObjectOutputStream(byteOutputStream)) {
 			objectOutputStream.writeObject(object);
 			return byteOutputStream.toByteArray();
 		} catch (IOException e) {
@@ -23,7 +29,7 @@ public class IOUtils {
 	}
 
 	public static <T extends Serializable> T deserialize(byte[] bytes) {
-		try (ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes)) {
+		try (var byteArrayInputStream = new ByteArrayInputStream(bytes)) {
 			return deserialize(byteArrayInputStream);
 		} catch (IOException e) {
 			throw new InternalException(e);
@@ -32,7 +38,7 @@ public class IOUtils {
 
 	@SuppressWarnings("unchecked")
 	public static <T extends Serializable> T deserialize(InputStream inputStream) {
-		try (ObjectInputStream objectOutputStream = new ObjectInputStream(inputStream)) {
+		try (var objectOutputStream = new ObjectInputStream(inputStream)) {
 			return (T) objectOutputStream.readObject();
 		} catch (ClassNotFoundException | IOException e) {
 			throw new InternalException(e);
@@ -40,8 +46,8 @@ public class IOUtils {
 	}
 
 	public static byte[] compress(byte[] bytes) {
-		try (ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-		     GZIPOutputStream gzipOutputStream = new GZIPOutputStream(byteArrayOutputStream)) {
+		try (var byteArrayOutputStream = new ByteArrayOutputStream();
+		     var gzipOutputStream = new GZIPOutputStream(byteArrayOutputStream)) {
 			gzipOutputStream.write(bytes);
 			gzipOutputStream.close();
 			return byteArrayOutputStream.toByteArray();
@@ -71,9 +77,9 @@ public class IOUtils {
 	}
 
 	public static byte[] decompress(byte[] bytes) {
-		try (GZIPInputStream gzipInputStream = new GZIPInputStream(new ByteArrayInputStream(bytes));
-		     ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream()) {
-			byte[] buffer = new byte[1024];
+		try (var gzipInputStream = new GZIPInputStream(new ByteArrayInputStream(bytes));
+		     var byteArrayOutputStream = new ByteArrayOutputStream()) {
+			byte[] buffer = new byte[DEFAULT_BYTE_BUFFER_SIZE];
 			int len;
 			while ((len = gzipInputStream.read(buffer)) > 0) {
 				byteArrayOutputStream.write(buffer, 0, len);
@@ -85,8 +91,8 @@ public class IOUtils {
 	}
 
 	public static String toString(InputStream inputStream) {
-		try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
-			byte[] buffer = new byte[4096];
+		try (var outputStream = new ByteArrayOutputStream()) {
+			byte[] buffer = new byte[DEFAULT_BYTE_BUFFER_SIZE];
 			int length;
 			while ((length = inputStream.read(buffer)) != -1) {
 				outputStream.write(buffer, 0, length);
@@ -98,9 +104,9 @@ public class IOUtils {
 	}
 
 	public static byte[] toBytes(InputStream inputStream) {
-		try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+		try (var outputStream = new ByteArrayOutputStream()) {
+			byte[] buffer = new byte[DEFAULT_BYTE_BUFFER_SIZE];
 			int length;
-			byte[] buffer = new byte[4096];
 			while ((length = inputStream.read(buffer)) != -1) {
 				outputStream.write(buffer, 0, length);
 			}
@@ -109,6 +115,113 @@ public class IOUtils {
 		} catch (IOException e) {
 			throw new InternalException(e);
 		}
+	}
+
+	public static boolean equalHex(byte value, String hexValue) {
+		String otherValue = toHex(value);
+		return hexValue.equalsIgnoreCase(otherValue);
+	}
+
+	public static String toHex(byte value) {
+		String hex = String.valueOf(HEX_VALUES[(value >> 4) & 0xf]) + HEX_VALUES[(value & 0xf)];
+		return hex.length() > 1 ? hex : "0" + hex;
+	}
+
+	public static String toHex(byte[] bytes) {
+		return toHex(bytes, null);
+	}
+
+	public static String toHex(byte[] bytes, @Null Character separator) {
+		var stringBuilder = new StringBuilder();
+		boolean useSeparator = separator != null;
+		for (byte b : bytes) {
+			String hex = Integer.toHexString(0xff & b);
+			if (hex.length() == 1) {
+				stringBuilder.append('0');
+			}
+			stringBuilder.append(hex);
+			if (useSeparator) {
+				stringBuilder.append(separator);
+			}
+		}
+		if (useSeparator) {
+			stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+		}
+		return stringBuilder.toString();
+	}
+
+	public static byte[] parseHex(String value) {
+		int length = value.length();
+		if (length % 2 != 0) {
+			throw new IllegalArgumentException("hexBinary needs to be even-length: " + value);
+		}
+		byte[] out = new byte[length / 2];
+		for (int i = 0; i < length; i +=2) {
+			int h = toDec(value.charAt(i));
+			int l = toDec(value.charAt(i + 1));
+			if (h ==-1 || l == -1) {
+				throw new IllegalArgumentException("contains illegal character for hexBinary: " + value);
+			}
+			out[i / 2] = (byte) (h * 16 + l);
+		}
+		return out;
+	}
+
+	public static int toDec(char value) {
+		if ('0' <= value && value <= '9' )
+			return value-'0';
+		if ('A' <= value && value <= 'F' )
+			return value-'A'+10;
+		if ('a' <= value && value <= 'f' )
+			return value-'a'+10;
+		return -1;
+	}
+
+	public static int toDec(byte v1, byte v2) {
+		return ((v1 & 0xff) << 8) | (v2 & 0xff);
+	}
+
+	public static int toDec(byte[] bytes) {
+		if (bytes.length > 4) {
+			throw new IllegalArgumentException("Array is too big");
+		}
+		int result = 0;
+		for (byte value : bytes) {
+			result = result << 8;
+			result = result | (value & 0xff);
+		}
+		return result;
+	}
+
+	public static int toBit(int value, int index) {
+		return (value >> index) & 1;
+	}
+
+	public static String hashSha256(File file) {
+		try (var inputStream = new FileInputStream(file)) {
+			var messageDigest = MessageDigest.getInstance("SHA-256");
+			byte[] buffer = new byte[DEFAULT_BYTE_BUFFER_SIZE];
+			int length;
+			while ((length = inputStream.read(buffer)) != -1) {
+				messageDigest.update(buffer, 0, length);
+			}
+			return toHex(messageDigest.digest());
+		} catch (Exception e) {
+			throw new InternalException(e);
+		}
+	}
+
+	public static String hashSha256(byte[] bytes) {
+		try {
+			var messageDigest = MessageDigest.getInstance("SHA-256");
+			return toHex(messageDigest.digest(bytes));
+		} catch (Exception e) {
+			throw new InternalException(e);
+		}
+	}
+
+	public static String hashSha256(String value) {
+		return hashSha256(value.getBytes(StandardCharsets.UTF_8));
 	}
 
 }
--- src/test/java/com/github/zubmike/core/utils/IOUtilsTest.java
+++ src/test/java/com/github/zubmike/core/utils/IOUtilsTest.java
@@ -12,6 +12,10 @@ import java.nio.charset.StandardCharsets;
 public class IOUtilsTest {
 
 	private static final String TEST_STRING = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.";
+	private static final String TEST_STRING_HEX = "4c6f72656d20497073756d2069732073696d706c792064756d6d792074657874206f6620746865207072696e74696e6720616e64207479706573657474696e6720696e6475737472792e";
+
+	private static final String TEST_RU_STRING = "Съешь ещё этих мягких французских булок.";
+	private static final String TEST_RU_STRING_HEX = "d0a1d18ad0b5d188d18c20d0b5d189d19120d18dd182d0b8d18520d0bcd18fd0b3d0bad0b8d18520d184d180d0b0d0bdd186d183d0b7d181d0bad0b8d18520d0b1d183d0bbd0bed0ba2e";
 
 	@Test
 	public void serializeDeserialize() {
@@ -30,5 +34,65 @@ public class IOUtilsTest {
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
+	}
+
+	@Test
+	public void hexValue() {
+		var hexValue = IOUtils.toHex(TEST_STRING.getBytes(StandardCharsets.UTF_8));
+		Assert.assertEquals(TEST_STRING_HEX, hexValue);
+		var textValue = new String(IOUtils.parseHex(hexValue), StandardCharsets.UTF_8);
+		Assert.assertEquals(TEST_STRING, textValue);
+
+		Assert.assertEquals("ab", IOUtils.toHex((byte) 0xab));
+		Assert.assertEquals("0f", IOUtils.toHex((byte) 0xf));
+	}
+
+	@Test
+	public void hexRuValue() {
+		var hexValue = IOUtils.toHex(TEST_RU_STRING.getBytes(StandardCharsets.UTF_8));
+		Assert.assertEquals(TEST_RU_STRING_HEX, hexValue);
+		var textValue = new String(IOUtils.parseHex(hexValue), StandardCharsets.UTF_8);
+		Assert.assertEquals(TEST_RU_STRING, textValue);
+	}
+
+	@Test
+	public void hexValueWithSeparator() {
+		var hex = IOUtils.toHex("qwerty".getBytes(StandardCharsets.UTF_8), ' ');
+		Assert.assertEquals("71 77 65 72 74 79", hex);
+	}
+
+	@Test
+	public void equalsByteAndHexValues() {
+		Assert.assertTrue(IOUtils.equalHex((byte) 0xab, "ab"));
+		Assert.assertTrue(IOUtils.equalHex((byte) 0xab, "ab".toUpperCase()));
+
+		Assert.assertFalse(IOUtils.equalHex((byte) 0xff, "fe"));
+	}
+
+	@Test
+	public void convertHexToInt() {
+		Assert.assertEquals(10, IOUtils.toDec('a'));
+		Assert.assertEquals(11, IOUtils.toDec('b'));
+	}
+
+	@Test
+	public void convertByteToInt() {
+		Assert.assertEquals(0xabcd, IOUtils.toDec((byte) 0xab, (byte) 0xcd));
+		Assert.assertEquals(0xabcd, IOUtils.toDec(new byte[] {(byte) 0xab, (byte) 0xcd}));
+		Assert.assertEquals(0xabcddcba, IOUtils.toDec(new byte[] {(byte) 0xab, (byte) 0xcd, (byte) 0xdc, (byte) 0xba}));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void convertLargeByteArrayToInt() {
+		IOUtils.toDec(new byte[] {1, 2, 3, 4, 5});
+	}
+
+	@Test
+	public void conventByteToBit() {
+		Assert.assertEquals(1, IOUtils.toBit(13, 0));
+		Assert.assertEquals(0, IOUtils.toBit(13, 1));
+		Assert.assertEquals(1, IOUtils.toBit(13, 2));
+		Assert.assertEquals(1, IOUtils.toBit(13, 3));
+		Assert.assertEquals(0, IOUtils.toBit(13, 4));
 	}
 }
